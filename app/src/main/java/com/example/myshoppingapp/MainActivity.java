6f72e4336a7632f64ffcad2c00811243923231df
kewalg <kewalgulve007@gmail.com>
2019-10-23 13:10:33 -0400

    backend of firebase

--- app/src/main/java/com/example/myshoppingapp/MainActivity.java
+++ app/src/main/java/com/example/myshoppingapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myshoppingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,23 +8,75 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtuser;
+    EditText edtpass;
+    Button submitbtn;
+    Button registerbtn;
+    FirebaseAuth mFireBaseAuth;
+    FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final EditText edtuser = findViewById(R.id.edtusername);
-        final EditText edtpass = findViewById(R.id.edtpassword);
-        Button submitbtn = findViewById(R.id.btnsubmit);
-        Button registerbtn = findViewById(R.id.btnregister);
-
+        edtuser = findViewById(R.id.edtusername);
+        edtpass = findViewById(R.id.edtpassword);
+        submitbtn = findViewById(R.id.btnsubmit);
+        registerbtn = findViewById(R.id.btnregister);
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser mFirebaseUser = mFireBaseAuth.getCurrentUser();
+                if (mFirebaseUser != null) {
+                    Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_SHORT).show();
+                    Intent i = new Intent(MainActivity.this, MenuActivity3.class);
+                    startActivity(i);
+                }
+            }
+        };
+        mFireBaseAuth = FirebaseAuth.getInstance();
         submitbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //add authentication from firebase
+                String user_login = edtuser.getText().toString();
+                String pass_login = edtpass.getText().toString();
+
+                if (user_login.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "username cannot be emtpy", Toast.LENGTH_SHORT).show();
+                } else if (pass_login.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "password cannot be empty", Toast.LENGTH_SHORT).show();
+                } else if (user_login.isEmpty() && pass_login.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "fields cannot be empty", Toast.LENGTH_SHORT).show();
+                } else if (!(user_login.isEmpty() && pass_login.isEmpty())) {
+                    mFireBaseAuth.signInWithEmailAndPassword(user_login, pass_login).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!(task.isSuccessful())){
+                                Toast.makeText(MainActivity.this, "login failed", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Intent i = new Intent(MainActivity.this, MenuActivity3.class) ;
+                                startActivity(i);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegisterActivity2.class);
+                startActivity(i);
             }
         });
     }
--- app/src/main/java/com/example/myshoppingapp/RegisterActivity2.java
+++ app/src/main/java/com/example/myshoppingapp/RegisterActivity2.java
@@ -1,14 +1,67 @@
 package com.example.myshoppingapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity2 extends AppCompatActivity {
+
+    EditText edtuser_reg;
+    EditText edtpass_reg;
+    Button registerbtn;
+    FirebaseAuth mFireBaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register2);
+        edtuser_reg = findViewById(R.id.edtusername_reg);
+        edtpass_reg = findViewById(R.id.edtpassword_reg);
+        registerbtn = findViewById(R.id.registerbtn_reg);
+
+        mFireBaseAuth = FirebaseAuth.getInstance();
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = edtuser_reg.getText().toString();
+                String pass = edtpass_reg.getText().toString();
+
+                if (user.isEmpty()){
+                    edtuser_reg.setError("Enter an email address");
+                    edtuser_reg.requestFocus();
+                }else if(pass.isEmpty()){
+                    edtpass_reg.setError("Enter a password");
+                    edtpass_reg.requestFocus();
+                }else if(user.isEmpty() && pass.isEmpty()){
+                    Toast.makeText(RegisterActivity2.this, "fields cannot be empty!", Toast.LENGTH_SHORT).show();
+                }else if(!(user.isEmpty() && pass.isEmpty())){
+                    mFireBaseAuth.createUserWithEmailAndPassword(user,pass).addOnCompleteListener(RegisterActivity2.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(RegisterActivity2.this, "Registeration unsuccesfull!", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(RegisterActivity2.this, "Registeration succesfull", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(RegisterActivity2.this, MainActivity.class));
+                            }
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(RegisterActivity2.this, "Error occured", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
