2b96ca7469e590aebd904bbedd2a405b2a13919e
kewalg <kewalgulve007@gmail.com>
2019-10-24 23:51:56 -0400

    listview implementation

--- app/src/main/java/com/example/myshoppingapp/MenuActivity3.java
+++ app/src/main/java/com/example/myshoppingapp/MenuActivity3.java
@@ -3,6 +3,13 @@ package com.example.myshoppingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 public class MenuActivity3 extends AppCompatActivity {
 
@@ -10,5 +17,21 @@ public class MenuActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu3);
+
+        ListView lv_food = findViewById(R.id.lv_groceries);
+        ArrayList<String> fooditems = new ArrayList<String>(asList("Chicken", "Milk", "Eggs", "Bread"));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fooditems);
+        lv_food.setAdapter(arrayAdapter);
+
+        ListView lv_clothes = findViewById(R.id.lv_clothes);
+        ArrayList<String> clothitems = new ArrayList<String>(asList("Shirts", "Tshirts", "Jeans", "Joggers"));
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, clothitems);
+        lv_clothes.setAdapter(arrayAdapter1);
+
+        ListView lv_electronics = findViewById(R.id.lv_electronics);
+        ArrayList<String> electronicitems = new ArrayList<String>(asList("Laptops", "Mobiles", "Earphones", "Speakers"));
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, electronicitems);
+        lv_electronics.setAdapter(arrayAdapter2);
+
     }
 }
